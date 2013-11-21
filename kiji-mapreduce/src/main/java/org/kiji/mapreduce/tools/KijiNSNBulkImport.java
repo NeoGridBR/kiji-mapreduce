e073b84483138918202b23a12160a80a0c15f811
Cristofer <cris.weber@gmail.com>
2013-11-21 15:14:51 -0200

    Operação de importação de documentos no repositório

---
+++ kiji-mapreduce/src/main/java/org/kiji/mapreduce/tools/KijiNSNBulkImport.java
@@ -0,0 +1,177 @@
+package org.kiji.mapreduce.tools;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.kiji.annotations.ApiAudience;
+import org.kiji.common.flags.Flag;
+import org.kiji.mapreduce.MapReduceJobInput;
+import org.kiji.mapreduce.MapReduceJobOutput;
+import org.kiji.mapreduce.bulkimport.KijiBulkImportJobBuilder;
+import org.kiji.mapreduce.bulkimport.impl.KijiBulkImporters;
+import org.kiji.mapreduce.output.DirectKijiTableMapReduceJobOutput;
+import org.kiji.mapreduce.output.HFileMapReduceJobOutput;
+import org.kiji.mapreduce.output.KijiTableMapReduceJobOutput;
+import org.kiji.mapreduce.tools.framework.JobTool;
+import org.kiji.mapreduce.tools.framework.MapReduceJobInputFactory;
+import org.kiji.mapreduce.tools.framework.MapReduceJobOutputFactory;
+import org.kiji.schema.Kiji;
+import org.kiji.schema.KijiTable;
+import org.kiji.schema.tools.KijiToolLauncher;
+import org.kiji.schema.tools.RequiredFlagException;
+import org.kiji.schema.util.ResourceUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Preconditions;
+
+@ApiAudience.Private
+public class KijiNSNBulkImport extends JobTool<KijiBulkImportJobBuilder> {
+  private static final Logger LOG = LoggerFactory.getLogger(KijiNSNBulkImport.class);
+
+  @Flag(name="importer", usage="KijiBulkImporter class to use")
+  private String mImporter = "";
+
+  /** {@inheritDoc} */
+  @Override
+  public String getName() {
+    return "nsn-bulk-import";
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public String getDescription() {
+    return "Bulk import metadata into Repository table";
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public String getCategory() {
+    return "Bulk";
+  }
+
+  /** Kiji instance where the output table lives. */
+  private Kiji mKiji;
+
+  /** KijiTable to import data into. */
+  private KijiTable mTable;
+
+  /** Job output. */
+  private KijiTableMapReduceJobOutput mOutput;
+
+  /** {@inheritDoc} */
+  @Override
+  protected void validateFlags() throws Exception {
+    // Do NOT call super.validateFlags()
+
+    if (mInputFlag.isEmpty()) {
+      throw new RequiredFlagException("input");
+    }
+    if (mImporter.isEmpty()) {
+      throw new RequiredFlagException("importer");
+    }
+    if (mOutputFlag.isEmpty()) {
+      throw new RequiredFlagException("output");
+    }
+
+    final MapReduceJobOutput mrJobOutput =
+        MapReduceJobOutputFactory.create().fromSpaceSeparatedMap(mOutputFlag);
+    Preconditions.checkArgument(mrJobOutput instanceof KijiTableMapReduceJobOutput,
+        "NSN-bulk-import jobs output format must be 'hfile' or 'kiji', but got output spec '%s'.",
+        mOutputFlag);
+    mOutput = (KijiTableMapReduceJobOutput) mrJobOutput;
+
+    Preconditions.checkArgument(mOutput.getOutputTableURI().getTable() != null,
+        "Specify the table to import data into with --output=...");
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  protected void setup() throws Exception {
+    super.setup();
+    mKiji = Kiji.Factory.open(mOutput.getOutputTableURI(), getConf());
+    mTable = mKiji.openTable(mOutput.getOutputTableURI().getTable());
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  protected void cleanup() throws IOException {
+    ResourceUtils.releaseOrLog(mTable);
+    ResourceUtils.releaseOrLog(mKiji);
+    super.cleanup();
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  protected KijiBulkImportJobBuilder createJobBuilder() {
+    return KijiBulkImportJobBuilder.create();
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  protected void configure(KijiBulkImportJobBuilder jobBuilder)
+      throws ClassNotFoundException, IOException {
+
+    // Resolve job input:
+    final MapReduceJobInput input =
+        MapReduceJobInputFactory.create().fromSpaceSeparatedMap(mInputFlag);
+
+    // Resolve job output:
+    final MapReduceJobOutput output =
+        MapReduceJobOutputFactory.create().fromSpaceSeparatedMap(mOutputFlag);
+    if (!(output instanceof KijiTableMapReduceJobOutput)) {
+      throw new RuntimeException(String.format(
+          "Invalid nsn-bulk-importer job output: '%s'; must output to a Kiji table.", mOutputFlag));
+    }
+    mOutput = (KijiTableMapReduceJobOutput) output;
+
+    // Configure job:
+    super.configure(jobBuilder);
+    jobBuilder
+        .withBulkImporter(KijiBulkImporters.forName(mImporter))
+        .withInput(input)
+        .withOutput(mOutput);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  protected int run(List<String> nonFlagArgs) throws Exception {
+    final int jobStatus = super.run(nonFlagArgs);
+
+    // TODO: Make this a method of job outputs?
+    if (mOutput instanceof DirectKijiTableMapReduceJobOutput) {
+      if (jobStatus == 0) {
+        LOG.info("NSN-bulk-import job for table {} completed successfully.",
+            mOutput.getOutputTableURI());
+      } else {
+        LOG.error("NSN-bulk-import job failed, output table {} may have partial writes.",
+            mOutput.getOutputTableURI());
+      }
+    } else if (mOutput instanceof HFileMapReduceJobOutput) {
+      if (jobStatus == 0) {
+        // Provide instructions for completing the bulk import.
+        LOG.info("NSN-bulk-import job completed successfully. "
+            + "HFiles may now be bulk-loaded into table {} with: {}",
+            mOutput.getOutputTableURI(),
+            String.format("kiji bulk-load --table=%s", mOutput.getOutputTableURI()));
+      } else {
+        LOG.error(
+            "Bulk-importer job failed: HFiles for table {} were not generated successfully.",
+            mOutput.getOutputTableURI());
+      }
+    } else {
+      LOG.error("Unknown job output format: {}", mOutput.getClass().getName());
+    }
+    return jobStatus;
+  }
+
+  /**
+   * Program entry point.
+   *
+   * @param args The command-line arguments.
+   * @throws Exception If there is an error.
+   */
+  public static void main(String[] args) throws Exception {
+    System.exit(new KijiToolLauncher().run(new KijiNSNBulkImport(), args));
+  }
+}
